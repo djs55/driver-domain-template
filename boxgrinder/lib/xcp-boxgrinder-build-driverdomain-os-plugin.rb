b15bdea8181ad00507b916ff5015df188533cde4
David Scott <dave.scott@eu.citrix.com>
2012-06-23 07:28:06 -0400

    Add driverdomain OS plugin

--- boxgrinder/lib/xcp-boxgrinder-build-driverdomain-os-plugin.rb
+++ boxgrinder/lib/xcp-boxgrinder-build-driverdomain-os-plugin.rb
@@ -1,3 +1,3 @@
 require 'xcp-boxgrinder-build-driverdomain-os-plugin/driverdomain'
 
-plugin :class => BoxGrinder::DriverDomainPlugin, :type => :os, :name => :driverdomain, :full_name  => "XCP driver domain plugin"
+plugin :class => BoxGrinder::DriverDomainPlugin, :type => :os, :versions => [ "5" ], :name => :driverdomain, :full_name  => "XCP driver domain plugin", :require_root => true
